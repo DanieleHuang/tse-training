d9d41f0b9ed6185df18f1c8696b05d349859c87f
Sumeet Bansal <sumeetbansal@gmail.com>
2018-05-08 00:37:38 -0700

    finished project 4.5

--- sprint-4/routes/add.js
+++ sprint-4/routes/add.js
@@ -1,20 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-var data = require('../chirps.json')
+const MongoClient = require('mongodb').MongoClient;
 
 router.get('/', function(req, res, next) {
 	res.render('add');
 });
 
 router.post('/', function(req, res, next) {
+
 	var tweet = req.body;
 	tweet.isRetweet = false;
 	tweet.comments = 0;
 	tweet.retweets = 0;
 	tweet.likes = 0;
-	data.chirps.unshift(tweet);
-	res.redirect('/');
+
+	var db, murl = 'mongodb://s3bansal:tc18@ds217350.mlab.com:17350/tse-training'
+
+	MongoClient.connect(murl, (err, client) => {
+		if (err) console.log(err);
+		db = client.db('tse-training');
+		db.collection('chirps').save(tweet, (err, result) => {
+			res.redirect('/');
+		});
+
+	});
+
 });
 
 module.exports = router;
--- sprint-4/routes/index.js
+++ sprint-4/routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+const MongoClient = require('mongodb').MongoClient;
+
 var data = require('../chirps.json')
 
 router.get('/', function(req, res, next) {
-	res.render('index', data);
+
+	var db, murl = 'mongodb://s3bansal:tc18@ds217350.mlab.com:17350/tse-training'
+
+	MongoClient.connect(murl, (err, client) => {
+		if (err) console.log(err);
+		db = client.db('tse-training');
+		db.collection('chirps').find().toArray(function(err, results) {
+			res.render('index', {"chirps": results});
+		});
+	});
+
 });
 
 module.exports = router;
