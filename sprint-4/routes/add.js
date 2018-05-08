833288061ca3d5c0284d1b11d4bf8ecf467d4cc7
Sumeet Bansal <sumeetbansal@gmail.com>
2018-05-07 23:57:34 -0700

    copied over proj4 from sprint 3

---
+++ sprint-4/routes/add.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+var data = require('../chirps.json')
+
+router.get('/', function(req, res, next) {
+	res.render('add');
+});
+
+router.post('/', function(req, res, next) {
+	var tweet = req.body;
+	tweet.isRetweet = false;
+	tweet.comments = 0;
+	tweet.retweets = 0;
+	tweet.likes = 0;
+	data.chirps.unshift(tweet);
+	res.redirect('/');
+});
+
+module.exports = router;
