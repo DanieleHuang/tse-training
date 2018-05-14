09aca717b51abacc55982c1d36d5bcadf80f0217
jiaqi huang <huangjiaqi2397056Hjq@gmail.com>
2018-05-14 00:47:32 -0700

    error in unable to find the db

--- sprint-4/routes/posts.js
+++ sprint-4/routes/posts.js
@@ -3,6 +3,18 @@ var router = express.Router();
 var data = require('../chirps.json')
 const fs = require('fs');
 
+
+const MongoClient = require('mongodb').MongoClient
+
+var db;
+MongoClient.connect("mongodb://<dbuser>:<dbpassword>@ds121960.mlab.com:21960/garyflyer", (err, client) => {
+    if (err) return console.log(err)
+    db = client.db('garyflyer') // whatever your database name is
+    app.listen(3000, () => {
+        console.log('listening on 3000')
+    })
+})
+
 /* GET /about/
  * Note that the path specified below ('') is
  * relative to the path from app.js ('/about'),
@@ -12,14 +24,18 @@ router.get('/', function(req, res, next) {
     res.render('posts'); // renders /views/about.hbs
 });
 
-router.post('/', function(req, res, next) {
+
+router.post('/', (req, res) => {
     var tweet = req.body;
     tweet.isRetweet = false;
     tweet.comments = 0;
     tweet.retweets = 0;
     tweet.likes = 0;
-    data.chirps.push(tweet);
-    res.render('index', data);
-});
+    db.collection('posters').save(tweet, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+        res.redirect('index', data)
+    })
+})
 
 module.exports = router;
