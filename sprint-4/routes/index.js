95c03bf21b78af82538ab72ad798f0c2689904a4
jiaqi huang <huangjiaqi2397056Hjq@gmail.com>
2018-05-15 22:09:30 -0700

    add sprint 4 finish

--- sprint-4/routes/index.js
+++ sprint-4/routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var data = require('../chirps.json')
+const MongoClient = require('mongodb').MongoClient;
 
-router.get('/', function(req, res, next) {
-    res.render('index', data);
+MongoClient.connect("mongodb://jiaqihuangjqh:2397056Hjq@ds121960.mlab.com:21960/garyflyer", (err,client) => {
+    if (err) return console.log(err);
+    db = client.db('garyflyer');
 });
+
+router.get('/', function(req, res) {
+    db.collection('posters').find({}).sort({'_id': -1}).toArray((err, result) => {
+        res.render('index', {"chirps": result});
+    });
+})
 
 
 module.exports = router;
